97b4139053aa2c182086f42bd42d57b79a020c16
AntonChernooki <119884716+AntonChernooki@users.noreply.github.com>
2022-12-06 22:39:28 +0300

    Update book_subscription.h

--- main/book_subscription.h
+++ main/book_subscription.h
@@ -1,1 +1,33 @@
 #pragma once
+#ifndef BOOK_SUBSCRIPTION_H
+#define BOOK_SUBSCRIPTION_H
+
+#include "constants.h"
+
+struct date
+{
+    int hours;
+    int minutes;
+};
+
+struct person
+{
+    char first_name[MAX_STRING_SIZE];
+    char middle_name[MAX_STRING_SIZE];
+    char last_name[MAX_STRING_SIZE];
+};
+struct Topics
+{
+    char tema[MAX_STRING_SIZE];
+};
+
+struct book_subscription
+{
+    person reader;
+    date start;
+    date finish;
+    Topics tema;
+    char title[MAX_STRING_SIZE];
+};
+
+#endif
